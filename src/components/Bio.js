184d3ed7d64081abb23026e6583427b6d452f1ea
Pruthvi P <pruthvi@ixigo.com>
2018-12-09 16:12:51 +0530

    First post

---
+++ src/components/Bio.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src='https://avatars0.githubusercontent.com/u/1709317?s=460&v=4'
+          alt={`Pruthvi P`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          <div>Written by <strong>Pruthvi P</strong> </div>
+          <div>Writing code for the airline industry
+          <a href ="http://www.travenues.com" target="_blank">
+          @Travenues(Ixigo)</a></div>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/hello-world/index.md
@@ -0,0 +1,8 @@
+---
+title: New Beginnings
+date: "2018-12-09T22:40:32.169Z"
+---
+
+This is my first blog. I am planning to write some simple blogposts on some frontend and travel stuffs.
+I have used gatsby to build this site, which was super easy. Will write a detailed post on how to get started with gatsby and make a simple blog,
+almost for free.
