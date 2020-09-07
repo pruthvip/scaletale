9d97882bfc00c5ed8a7dc8570f0c5e6c98259596
atp <pruthvip15@gmail.com>
2020-09-07 13:47:43 +0530

    Update GA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-130647568-1',
+        trackingId: 'UA-177213550-1',
       },
     },
     `gatsby-plugin-feed`,
